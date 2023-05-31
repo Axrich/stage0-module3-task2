7246b50fa5b0f098480ab58f68bf84217559437a
Axrich <116589250+Axrich@users.noreply.github.com>
2023-05-31 09:03:37 +0500

    Update NamingConvention.java

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -1,15 +1,15 @@
 package lang.print.gaps.task2;
 
 public class NamingConvention {
-    public static void main(String[] args) {
-       int snake_case_variable = 10;
-        int variableOne = 1;
-        int VariableTwo = 2;
-        int variable_three = 3;
-        final int VARIABLE_FOUR = 4;
-        final int VARIABLE_FIVE = 5;
-        int VariableSix = 6;
-        int VariableSeven = 7;
-        int VariableEight = 8;
-    }
+public static void main(String[] args) {
+int variable = 10;
+int variableOne = 1;
+int variableTwo = 2;
+int variableThree = 3;
+final int VARIABLE_FOUR = 4;
+final int VARIABLE_FIVE = 5;
+int variableSix = 6;
+int variableSeven = 7;
+int variableEight = 8;
+}
 }
