333d23bd18021a124900545049e94687400bf922
Axrich <116589250+Axrich@users.noreply.github.com>
2023-06-01 11:12:05 +0500

    Update NamingConvention.java

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -1,15 +1,12 @@
 package lang.print.gaps.task2;
 
 public class NamingConvention {
-public static void main(String[] args) {
-int variable = 10;
-int variableOne = 1;
-int variableTwo = 2;
-int variableThree = 3;
-final int VARIABLE_FOUR = 4;
-final int VARIABLE_FIVE = 5;
-int variableSix = 6;
-int variableSeven = 7;
-int variableEight = 8;
-}
+    public static void main(String[] args) {
+        int myVariable = 10;
+        int myVariableOne = 1;
+        int myVariableTwo = 2;
+        int myVariableThree = 3;
+        final int VARIABLE_FOURTH = 4;
+        final int VARIABLE_FIFTH = 5;
+    }
 }
