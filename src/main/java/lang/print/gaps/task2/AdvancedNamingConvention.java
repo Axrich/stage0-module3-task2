c844d963dddef12c27d07cd28043c5ed1abb7a91
Axrich <116589250+Axrich@users.noreply.github.com>
2023-06-02 16:38:50 +0500

    Update AdvancedNamingConvention.java

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,19 +1,19 @@
 package lang.print.gaps.task2;
 
-public class AdvancedNamingConvention {
-
-    private static final int adultAge = 18;
+public class advancedNamingConvention {
+    private static final int ADULT_AGE = 18;
 
     private int age;
     private int phoneNumber;
 
     void callToFriend() {
         //some code
+        age = 11;
     }
 
     void callByNumber(int number) {
         //some code
+        phoneNumber = number;
     }
+
 }
-
-
